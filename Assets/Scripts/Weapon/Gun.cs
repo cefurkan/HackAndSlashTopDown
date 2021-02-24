d1206cbb05b5542a4f8eb14e9e56db405f960b1e
Furkan Ceylan <furkancylan@gmail.com>
2021-02-24 19:07:34 +0300

    IRangedWeapons added

--- Assets/Scripts/Weapon/Gun.cs
+++ Assets/Scripts/Weapon/Gun.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun : Weapons
+public class Gun : Weapons, IRangedWeapons
 {
+    public GameObject Projectile { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    public void Fire()
+    {
+       // Fire scripts
+    }
+
     public override void HeavyAttack()
     {
         throw new System.NotImplementedException();
@@ -17,5 +24,9 @@ public class Gun : Weapons
     public override void SpecialAttack()
     {
         throw new System.NotImplementedException();
+    }
+    public override void AttackInputs()
+    {
+        base.AttackInputs();
     }
 }
---
+++ Assets/Scripts/Weapon/IRangedWeapons.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IRangedWeapons
+{
+     GameObject Projectile { get; set; }
+
+     void Fire();
+}
